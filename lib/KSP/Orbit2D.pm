c5b0cf81383bc2696d1e5ed884d7c4083e234104
Pietro Cagnoni <pietro.cagnoni@gmail.com>
2021-04-12 12:17:08 +0200

    working on open orbits

--- lib/KSP/Orbit2D.pm
+++ lib/KSP/Orbit2D.pm
@@ -20,7 +20,7 @@ sub BUILD {
 }
 
 sub _need_ellipse {
-	$_[0]->e < 1 or croak "not allowed for open orbit";
+	$_[0]->e() < 1 or confess "not allowed for open orbit";
 }
 
 sub a { # major semiaxis
@@ -54,6 +54,7 @@ sub ap { # apoapsis height
 sub v_from_vis_viva {
 	my ($self, $h) = @_;
 	my $r = $h + $self->body->radius;
+	# $r or confess "division by zero";
 	sqrt($self->body->mu * (2 / $r - $self->inv_a))
 }
 
@@ -62,14 +63,24 @@ sub vmax {
 	$self->v_from_vis_viva($self->pe())
 }
 
+sub vmin {
+	my ($self) = @_;
+	$self->e() < 1 ?
+		$self->v_from_vis_viva($self->ap()) :
+		sqrt(-$self->body->mu() * $self->inv_a())
+}
+
 sub desc {
 	my ($self) = @_;
-	sprintf "[%s;pe=%4g;ap=%4g;vmax=%g;T=%s]",
-		$self->body->name(),
-		$self->pe(),
-		$self->ap(),
-		$self->vmax(),
-		KSP::Time->new($self->T)->pretty_interval()
+	my $open = $self->e() >= 1;
+	my @d = ();
+	push @d, $self->body->name();
+	push @d, sprintf("pe=%g", $self->pe());
+	$open or push @d, sprintf("ap=%g", $self->ap());
+	push @d, sprintf("vmax=%g", $self->vmax());
+	push @d, sprintf("vmin=%g", $self->vmin());
+	$open or push @d, KSP::Time->new($self->T)->pretty_interval();
+	"[" . join(";", @d) . "]"
 }
 
 1;
--- t/05-orbit.t
+++ t/05-orbit.t
@@ -10,8 +10,13 @@ use Data::Dump qw(dump);
 
 warn "\n";
 
+my $k = KSP::Body->get("Kerbin");
+warn KSP::Orbit2D->new($k, -$k->radius() * 10, 1.1)->desc(), "\n";
+
+warn "\n";
+
 foreach my $b (sort { $a->name() cmp $b->name() } KSP::Body->all()) {
-	warn $b->lowOrbit()->desc(), "\n";
-	warn $b->highOrbit()->desc(), "\n";
+	warn "L:", $b->lowOrbit()->desc(), "\n";
+	warn "H:", $b->highOrbit()->desc(), "\n";
 }
 
